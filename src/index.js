f40f6b7feb889c8d7bf41efd093638f857533805
Elio Neto <elioneto@MacBook-Pro-de-Elio.local>
2019-08-22 15:44:25 -0300

    Request movies

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Welcome from './welcome/welcome';
+import MyRequest from './Request/request';
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
             <Route path="/" exact={true} component={Welcome}/>
+            <Route path="/request" component={MyRequest}/>
         </Switch>
     </ BrowserRouter>, 
 document.getElementById('root')
