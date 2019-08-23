23d31398cf2baf83b4c0230bfeb1183256438581
Elio Neto <elioneto@189-039-008-046.static.spo.ctbc.com.br>
2019-08-23 12:02:00 -0300

    Done and Fail pages

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Welcome from './welcome/welcome';
 import MyRequest from './Request/request';
 import ToDo from './ToDo/todo';
+import Done from './Done/done';
+import Failure from './Fail/failure';
 
 ReactDOM.render(
     <BrowserRouter>
@@ -12,6 +14,8 @@ ReactDOM.render(
             <Route path="/" exact={true} component={Welcome}/>
             <Route path="/request" component={MyRequest}/>
             <Route path="/todo" component={ToDo}/>
+            <Route path="/done" component={Done}/>
+            <Route path="/fail" component={Failure}/>
         </Switch>
     </ BrowserRouter>, 
 document.getElementById('root')
