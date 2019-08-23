8d2bb9558d30b86c5aaa7c1b1c5edd8ff0f28af3
Elio Neto <elioneto@189-039-008-046.static.spo.ctbc.com.br>
2019-08-23 12:17:59 -0300

    Ajuste no navbar

--- src/Components/header.js
+++ src/Components/header.js
@@ -6,16 +6,19 @@ class Header extends Component {
   render() {
       return (
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand>Get Movies</Navbar.Brand>
-                <Nav className="mr-auto">
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand>Get Movies</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
                 <Nav.Link href="/">Welcome</Nav.Link>
                 <Nav.Link href="/request">Request</Nav.Link>
                 <Nav.Link href="/todo">ToDo</Nav.Link>
                 <Nav.Link href="/done">Done</Nav.Link>
                 <Nav.Link href="/fail">Failure</Nav.Link>
-                </Nav>
-            </Navbar>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </div>
       );
   }
